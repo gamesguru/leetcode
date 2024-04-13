036553ebb0983b30d641c0a29a6e7afe321bfb23
Shane Jaroch <chown_tee@proton.me>
2024-04-13 10:56:31 -0400

    wip nothing daily maximal-rectangle

--- daily/maximal-rectangle.cpp
+++ daily/maximal-rectangle.cpp
@@ -1,7 +1,19 @@
 class Solution {
 public:
     int maximalRectangle(vector<vector<char>>& matrix) {
+        int a = 0;
+        int height = matrix.size();
+        int width = matrix[0].size();
 
+        for (int i=0; i<height; i++) {
+            for (int j=0; j<width; j++;) {
+                int w = 0;
+                int i2 = i;
+                while (i2 < height and )
+            }
+        }
+
+        return a;
     }
 };
 
