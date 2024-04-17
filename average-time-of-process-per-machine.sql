b2c5b29f84ce35570ec74898653eb32168942a14
Shane Jaroch <chown_tee@proton.me>
2024-04-17 14:49:01 -0400

    test case 1 sql

--- average-time-of-process-per-machine.sql
+++ average-time-of-process-per-machine.sql
@@ -1,21 +1,15 @@
 SELECT
-  machine_id,
-  sum(timestamp) AS sum_starts
+  Activity.machine_id,
+  round((e.end_total - s.start_total) / 2, 3) AS processing_time
+  -- round(sum(timestamp), 3) AS processing_time
   -- round(sum(timestamp) / count(*), 3) AS processing_time
 FROM
-  Activity
+  Activity,
+  (select machine_id, sum(timestamp) as start_total from Activity where activity_type='start' group by machine_id) as s,
+  (select machine_id, sum(timestamp) as end_total from Activity where activity_type='end' group by machine_id) as e
 WHERE
-  activity_type = 'start'
-GROUP BY
-  machine_id;
-
-SELECT
-  machine_id,
-  sum(timestamp) AS sum_ends
-FROM
-  Activity
-WHERE
-  activity_type = 'end'
+  s.machine_id = Activity.machine_id
+  AND e.machine_id = Activity.machine_id
 GROUP BY
   machine_id;
 
@@ -38,3 +32,19 @@ insert into Activity (machine_id, process_id, activity_type, timestamp) values (
 insert into Activity (machine_id, process_id, activity_type, timestamp) values ('2', '0', 'end', '4.512');
 insert into Activity (machine_id, process_id, activity_type, timestamp) values ('2', '1', 'start', '2.5');
 insert into Activity (machine_id, process_id, activity_type, timestamp) values ('2', '1', 'end', '5');
+
+-- Test case 2:
+| machine_id | process_id | activity_type | timestamp |
+| ---------- | ---------- | ------------- | --------- |
+| 4          | 0          | end           | 98.227    |
+| 0          | 0          | start         | 89.478    |
+| 2          | 0          | start         | 4.06      |
+| 4          | 0          | start         | 35.617    |
+| 0          | 0          | end           | 93.686    |
+| 1          | 0          | end           | 97.821    |
+| 1          | 0          | start         | 73.793    |
+| 3          | 0          | end           | 85.537    |
+| 3          | 0          | start         | 35.138    |
+| 2          | 0          | end           | 48.225    |
+| 5          | 0          | start         | 22.284    |
+| 5          | 0          | end           | 53.926    |
