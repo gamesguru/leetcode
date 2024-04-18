edd7f2178a49f665c54cb2e97bca17c5d46e0e6f
Shane Jaroch <chown_tee@proton.me>
2024-04-17 20:34:57 -0400

    solve 2

--- average-time-of-process-per-machine.sql
+++ average-time-of-process-per-machine.sql
@@ -1,21 +1,69 @@
+-- Difficulty: easy
+
+-- SELECT
+--   a.machine_id,
+--   round(avg(b.timestamp - a.timestamp), 3) AS processing_time
+-- FROM
+--   Activity a,
+--   Activity b
+-- WHERE
+--   a.machine_id = b.machine_id
+--   AND a.process_id = b.process_id
+--   AND a.activity_type = 'start'
+--   AND b.activity_type = 'end'
+-- GROUP BY
+--   machine_id;
+
+-- Runtime 456 ms
+-- Beats 52.12% of users with MySQL
+
 SELECT
-  Activity.machine_id,
-  round((e.end_total - s.start_total) / 2, 3) AS processing_time
-  -- round(sum(timestamp), 3) AS processing_time
-  -- round(sum(timestamp) / count(*), 3) AS processing_time
+  machine_id,
+  round(avg(
+      CASE WHEN activity_type = 'start' THEN
+        timestamp * -1
+      ELSE
+        timestamp
+      END) * 2, 3) AS processing_time
 FROM
-  Activity,
-  (select machine_id, sum(timestamp) as start_total from Activity where activity_type='start' group by machine_id) as s,
-  (select machine_id, sum(timestamp) as end_total from Activity where activity_type='end' group by machine_id) as e
-WHERE
-  s.machine_id = Activity.machine_id
-  AND e.machine_id = Activity.machine_id
+  Activity
 GROUP BY
   machine_id;
 
-  -- (SELECT machine_id, sum(timestamp) as sum_starts FROM Activity WHERE activity_type='start' AND machine_id=Activity.machine_id) x;
+-- Runtime 395ms
+-- Beats 78.69% of users with MySQL
 
--- Difficulty: easy
+-- Test case 3
+-- | machine_id | process_id | activity_type | timestamp |
+-- | ---------- | ---------- | ------------- | --------- |
+-- | 0          | 0          | start         | 0.712     |
+-- | 0          | 0          | end           | 1.52      |
+-- | 0          | 1          | start         | 3.14      |
+-- | 0          | 1          | end           | 4.12      |
+-- | 1          | 0          | start         | 0.55      |
+-- | 1          | 0          | end           | 1.55      |
+-- | 1          | 1          | start         | 0.43      |
+-- | 1          | 1          | end           | 1.42      |
+-- | 2          | 0          | start         | 4.1       |
+-- | 2          | 0          | end           | 4.512     |
+-- | 2          | 1          | start         | 2.5       |
+-- | 2          | 1          | end           | 5         |
+
+-- Test case 2
+-- | machine_id | process_id | activity_type | timestamp |
+-- | ---------- | ---------- | ------------- | --------- |
+-- | 4          | 0          | end           | 98.227    |
+-- | 0          | 0          | start         | 89.478    |
+-- | 2          | 0          | start         | 4.06      |
+-- | 4          | 0          | start         | 35.617    |
+-- | 0          | 0          | end           | 93.686    |
+-- | 1          | 0          | end           | 97.821    |
+-- | 1          | 0          | start         | 73.793    |
+-- | 3          | 0          | end           | 85.537    |
+-- | 3          | 0          | start         | 35.138    |
+-- | 2          | 0          | end           | 48.225    |
+-- | 5          | 0          | start         | 22.284    |
+-- | 5          | 0          | end           | 53.926    |
 
 -- Test case 1:
 Create table If Not Exists Activity (machine_id int, process_id int, activity_type ENUM('start', 'end'), timestamp float);
@@ -32,19 +80,3 @@ insert into Activity (machine_id, process_id, activity_type, timestamp) values (
 insert into Activity (machine_id, process_id, activity_type, timestamp) values ('2', '0', 'end', '4.512');
 insert into Activity (machine_id, process_id, activity_type, timestamp) values ('2', '1', 'start', '2.5');
 insert into Activity (machine_id, process_id, activity_type, timestamp) values ('2', '1', 'end', '5');
-
--- Test case 2:
-| machine_id | process_id | activity_type | timestamp |
-| ---------- | ---------- | ------------- | --------- |
-| 4          | 0          | end           | 98.227    |
-| 0          | 0          | start         | 89.478    |
-| 2          | 0          | start         | 4.06      |
-| 4          | 0          | start         | 35.617    |
-| 0          | 0          | end           | 93.686    |
-| 1          | 0          | end           | 97.821    |
-| 1          | 0          | start         | 73.793    |
-| 3          | 0          | end           | 85.537    |
-| 3          | 0          | start         | 35.138    |
-| 2          | 0          | end           | 48.225    |
-| 5          | 0          | start         | 22.284    |
-| 5          | 0          | end           | 53.926    |
---
+++ employee-bonus.sql
@@ -0,0 +1,25 @@
+-- Difficulty: easy
+SELECT
+  name,
+  bonus
+FROM
+  Employee
+  LEFT JOIN Bonus ON Employee.empId = Bonus.empId
+WHERE
+  bonus < 1000
+  OR bonus IS NULL;
+
+-- Runtime 1561 ms
+-- Beats 95.03% of users with MySQL
+
+-- Test case 1
+Create table If Not Exists Employee (empId int, name varchar(255), supervisor int, salary int);
+Create table If Not Exists Bonus (empId int, bonus int);
+Truncate table Employee;
+insert into Employee (empId, name, supervisor, salary) values ('3', 'Brad', 'None', '4000');
+insert into Employee (empId, name, supervisor, salary) values ('1', 'John', '3', '1000');
+insert into Employee (empId, name, supervisor, salary) values ('2', 'Dan', '3', '2000');
+insert into Employee (empId, name, supervisor, salary) values ('4', 'Thomas', '3', '4000');
+Truncate table Bonus;
+insert into Bonus (empId, bonus) values ('2', '500');
+insert into Bonus (empId, bonus) values ('4', '2000');
