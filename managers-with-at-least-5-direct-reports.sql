36eba5eb73a9f80c455b461b078e3dee654c0c4a
Shane Jaroch <chown_tee@proton.me>
2024-04-17 22:02:08 -0400

    add test case

--- managers-with-at-least-5-direct-reports.sql
+++ managers-with-at-least-5-direct-reports.sql
@@ -1,43 +1,38 @@
 -- Difficulty: medium
 
--- select name from Employee where (select count(managerId) from Employee where id = managerId) IS NULL;
-
--- SELECT
---   e1.id,
---   e1.name,
---   e1.managerId,
---   (select count(id) as n_emp from Employee where e1.managerId = Employee.id) x
--- --   x.n_emp
--- FROM
---   Employee e1
--- -- LEFT JOIN Employee e2 on e1.id=e2.managerId
--- -- WHERE
--- --   x.n_emp >= 0
--- GROUP BY
---   id,
---   name,
--- --   x.n_emp,
---   managerId;
-
 SELECT
-  e1.id,
-  e1.name,
-  e2.managerId
---   x.n_emp
-FROM
-  Employee e1
--- LEFT JOIN Employee e2 on e1.id=e2.managerId
--- WHERE
---   x.n_emp >= 0
-LEFT OUTER JOIN Employee e2 ON e1.id = e2.managerId
---   AND count(e2.managerId) > 4
-GROUP BY
-  id,
-  name,
---   x.n_emp,
-  managerId;
+  -- x.managerId
+  e2.name
+FROM (
+  SELECT
+    managerId,
+    count(managerId) AS n_emps
+  FROM
+    Employee
+  GROUP BY
+    managerId) x
+  LEFT JOIN Employee e2 ON x.managerId = e2.id
+WHERE
+  x.n_emps >= 5;
 
--- select count(id) from Employee where managerId = 101;
+-- Test case 10/11
+-- | id  | name  | department | managerId |
+-- | --- | ----- | ---------- | --------- |
+-- | 101 | John  | A          | null      |
+-- | 102 | Dan   | A          | 100       |
+-- | 103 | James | A          | 100       |
+-- | 104 | Amy   | A          | 100       |
+-- | 105 | Anne  | A          | 100       |
+-- | 106 | Ron   | B          | 100       |
+
+-- output
+-- | name |
+-- | ---- |
+-- | null |
+
+-- expected
+-- | name |
+-- | ---- |
 
 -- Test case 1
 -- Create table If Not Exists Employee (id int, name varchar(255), department varchar(255), managerId int);
