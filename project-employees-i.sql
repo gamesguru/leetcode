81d28486283103abdac1bad31064da12236c5469
Shane Jaroch <chown_tee@proton.me>
2024-04-20 02:17:24 -0400

    solve 1 more easy sql

---
+++ project-employees-i.sql
@@ -0,0 +1,10 @@
+-- Difficulty: easy
+SELECT
+  p.project_id,
+  round(sum(experience_years) / count(experience_years), 2) AS average_years
+FROM
+  Project p
+  LEFT JOIN Employee e ON p.employee_id = e.employee_id
+GROUP BY
+  p.project_id
+;
